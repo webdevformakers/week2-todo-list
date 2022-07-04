effe99889293297df16e091b0acc8e23afe582de
Samson Zhang <wwsamson@yahoo.com>
2022-07-03 21:39:07 -0400

    add todo list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
+
 export default function Index() {
+    const [items, setItems] = useState<{name: string, completed: boolean}[]>([]);
+    const [newItemName, setNewItemName] = useState("");
+
+    function onAdd() {
+        const newItem = {
+            name: newItemName,
+            completed: false,
+        };
+
+        setItems([newItem, ...items]);
+
+        setNewItemName("");
+    }
+
+    function onDone(i: number) {
+        let newItems = [...items];
+        newItems[i].completed = !newItems[i].completed;
+        setItems(newItems);
+    }
+
+    function onDelete(i: number) {
+        let newItems = [...items];
+        newItems.splice(i,1);
+        setItems(newItems);
+    }
+
+    function onDeleteAll() {
+        setItems([]);
+    }
+
+    function onDeleteAllCompleted() {
+        setItems(items.filter(d => !d.completed));
+    }
+
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+            <div className="max-w-sm mx-auto my-16">
+                <input className="p-2 border" value={newItemName} onChange={e => setNewItemName(e.target.value)} />
+                <button className="p-2 bg-gray-700 text-white" onClick={onAdd}>Add</button>
+                <button className="p-2 bg-red-500 text-white" onClick={onDeleteAll}>Delete all</button>
+                <button className="p-2 bg-green-100" onClick={onDeleteAllCompleted}>Delete all completed</button>
+                {items.map((d, i) => (
+                    <div className="p-4 shadow-md my-8">
+                        <p className={"mb-4 " + (d.completed ? "line-through text-gray-500" : "")}>{d.name}</p>
+                        <button className="p-2 mr-2 bg-green-700 text-white" onClick={() => onDone(i)}>Mark {d.completed ? "not done" : "done"}</button>
+                        <button className="p-2 bg-red-500 text-white" onClick={() => onDelete(i)}>Delete</button>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
